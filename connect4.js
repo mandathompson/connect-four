5d900ef92f0f2855b52e7d90b015dacbabcf6864
Amanda <mandathompson@yahoo.com>
2020-07-08 20:51:22 -0400

    Update connect4.js

--- connect4.js
+++ connect4.js
@@ -12,11 +12,10 @@ let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
- *    board = array of rows, each row is array of cells  (board[y][x])
+ *   board = array of rows, each row is array of cells  (board[y][x])
  */
-
 function makeBoard() {
-  for (let y = 0; y < HEIGHT; y++){
+  for (let y = 0; y < HEIGHT; y++) {
     board.push(Array.from({ length: WIDTH }));
   }
 }
@@ -24,28 +23,32 @@ function makeBoard() {
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  const board = document.getElementById('board');
-  // make the column top area; this is the clickable "drop spot" to add a piece to the column 
-  const top = document.createElement("tr");
-  top.setAttribute("id", "column-top");
-  top.addEventListener("click", handleClick);
+  const board = document.querySelector('#board');
+
+  // make column tops (clickable area for adding a piece to that column)
+  const top = document.createElement('tr');
+  top.setAttribute('id', 'column-top');
+  top.addEventListener('click', handleClick);
 
   for (let x = 0; x < WIDTH; x++) {
-    let headCell = document.createElement("td");
-    headCell.setAttribute("id", x);
+    const headCell = document.createElement('td');
+    headCell.setAttribute('id', x);
     top.append(headCell);
   }
-  htmlBoard.append(top);
 
-  // TODO: add comment for this code
+  board.append(top);
+
+  // make main part of board
   for (let y = 0; y < HEIGHT; y++) {
-    const row = document.createElement("tr");
+    const row = document.createElement('tr');
+
     for (let x = 0; x < WIDTH; x++) {
-      const cell = document.createElement("td");
-      cell.setAttribute("id", `${y}-${x}`);
+      const cell = document.createElement('td');
+      cell.setAttribute('id', `${y}-${x}`);
       row.append(cell);
     }
-    htmlBoard.append(row);
+
+    board.append(row);
   }
 }
 
@@ -58,8 +61,6 @@ function findSpotForCol(x) {
     }
   }
   return null;
-}
-
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
@@ -77,7 +78,7 @@ function placeInTable(y, x) {
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -93,20 +94,21 @@ function handleClick(evt) {
   }
 
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
   board[y][x] = currPlayer;
   placeInTable(y, x);
-
+  
   // check for win
   if (checkForWin()) {
     return endGame(`Player ${currPlayer} won!`);
   }
-
+  
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
-
+  if (board.every(row => row.every(cell => cell))) {
+    return endGame('Tie!');
+  }
+    
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  currPlayer = currPlayer === 1 ? 2 : 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -127,15 +129,16 @@ function checkForWin() {
     );
   }
 
-  // TODO: read and understand this code. Add comments to help you.
-
   for (let y = 0; y < HEIGHT; y++) {
     for (let x = 0; x < WIDTH; x++) {
+      // get "check list" of 4 cells (starting here) for each of the different
+      // ways to win
       const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
       const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
       const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
       const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
+      // find winner (only checking each win-possibility as needed)
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
       }
